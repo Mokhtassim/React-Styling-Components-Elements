70796fde8faf606d5d905233a3f88de47c358548
Mokhtassim <brahimmokhtassim@gmail.com>
2020-01-11 16:52:27 +0100

    Using Radium For Media Queries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
   state = {
@@ -88,13 +88,14 @@ class App extends Component {
      }
     }
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
         <button style={style} onClick={this.showPersons}>Show Persons</button>
         {Persons}
-        
       </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,13 @@ import './Person.css';
 import Radium from 'radium';
 
 const person = (props) => {
+ const style={
+     '@media (min-width: 500px)': {
+         width: '450px'
+     }
+ }
     return (
-        <div className="person">
+        <div className="person" style={style}>
             <h2 onClick={props.click}>i'm {props.name}, Age: {props.age}</h2>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed}/>
