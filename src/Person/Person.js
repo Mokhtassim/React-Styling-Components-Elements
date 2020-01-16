4fda60f938a8622759dcd6ad2ad17361fe545d9d
Mokhtassim <brahimmokhtassim@gmail.com>
2020-01-16 22:10:25 +0100

    Styling More on styled-components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,29 +1,25 @@
 import React from 'react';
 // import './Person.css';
 // import Radium from 'radium';
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-const styledDiv = styled.div`
+const StyledDiv = styled.div`
     width: 60%;
     margin: 16px auto;
     border: 2px solid #333;
     box-shadow: 4px 4px 4px 4px #ccc;
     padding: 6px;
     text-align: center;
-
-    @media (min-width: 500px): {
-        width: '450px'
-    }
 `;
 
 const person = (props) => {
     return (
         // <div className="person" style={style}>
-        <styledDiv>
+        <StyledDiv>
             <h2 onClick={props.click}>i'm {props.name}, Age: {props.age}</h2>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed}/>
-        </styledDiv>
+        </StyledDiv>
     )
     
 };
