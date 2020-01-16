709b8571878e0ca3c14f2e27eebb1916bdcd40fa
Mokhtassim <brahimmokhtassim@gmail.com>
2020-01-16 22:42:48 +0100

    Styling dynamic content in styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import styled from 'styled-components';
 // import Radium, {StyleRoot} from 'radium';
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alr ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+
+  &:hover {
+    background-color: ${props => props.alr ? 'salmon' : 'lightgreen'};
+    color: black
+  }
+`;
 
 class App extends Component {
   state = {
@@ -46,17 +60,17 @@ class App extends Component {
 
   render () {
     //style
- const style = {
-  backgroundColor: 'green',
-  color: 'white',
-  font: 'inherit',
-  border: '1px solid blue',
-  padding: '8px',
-  ':hover':{
-    backgroundColor:'lightgreen',
-    color:'black'
-  }
-   }
+//  const style = {
+//   backgroundColor: 'green',
+//   color: 'white',
+//   font: 'inherit',
+//   border: '1px solid blue',
+//   padding: '8px',
+//   ':hover':{
+//     backgroundColor:'lightgreen',
+//     color:'black'
+//   }
+//    }
    //add classes
    let classes = [];
    if(this.state.persons.length <= 2)
@@ -81,17 +95,17 @@ class App extends Component {
        })}
      </div> );
      
-     style.backgroundColor="red";
-     style[':hover']= {
-       backgroundColor: 'salmon',
-       color:'black'
-     }
+    //  style.backgroundColor="red";
+    //  style[':hover']= {
+    //    backgroundColor: 'salmon',
+    //    color:'black'
+    //  }
     }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button style={style} onClick={this.showPersons}>Show Persons</button>
+        <StyledButton alr={this.state.showPersons} onClick={this.showPersons}>Show Persons</StyledButton>
         {Persons}
       </div>
     );
